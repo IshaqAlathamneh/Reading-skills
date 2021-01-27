8c07b0f5dad71db78c8523b85ce72b35396f1e07
IshaqAlathamneh <ishaqalathamneh@gmail.com>
2021-01-27 15:17:23 +0200

    Super Finish

---
+++ app.js
@@ -0,0 +1,6 @@
+var birth = prompt('What is your birth year')
+var age = 2021 - birth
+
+if(age<22){document.write('Go to your school baby')}
+else if(age>=23){document.write('you are elderly'}
+else{document.write('you are the best')} 
---
+++ pageColor.js
@@ -0,0 +1,2 @@
+var color = prompt("What's your favorite color")
+document.write('<style> body {background-color: '+ color +";} </style>")
